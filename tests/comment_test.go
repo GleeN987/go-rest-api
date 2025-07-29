4a2796b0962dd85c551f43a55c4cb23cd1cf89bd
GleeN987 <gleenmarcin@gmail.com>
2025-07-29 14:32:03 +0200

    Additional tests

--- tests/comment_test.go
+++ tests/comment_test.go
@@ -4,6 +4,7 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
@@ -41,5 +42,35 @@ func TestPostComment(t *testing.T) {
 			Post("http://localhost:8080/api/v1/comment")
 		assert.NoError(t, err)
 		assert.Equal(t, 401, response.StatusCode())
+	})
+}
+
+func TestGetComment(t *testing.T) {
+	t.Run("can get comment", func(t *testing.T) {
+		client := resty.New()
+		postResponse, err := client.R().
+			SetBody(`{"slug": "ss", "author": "kc", "body": "bleh"}`).
+			SetHeader("Authorization", "bearer "+createToken()).
+			Post("http://localhost:8080/api/v1/comment")
+		assert.NoError(t, err)
+		assert.Equal(t, 200, postResponse.StatusCode())
+
+		var createdComment struct {
+			ID string `json:"id"`
+		}
+
+		err = json.Unmarshal(postResponse.Body(), &createdComment)
+		assert.NoError(t, err)
+
+		getResponse, err := client.R().Get("http://localhost:8080/api/v1/comment/" + createdComment.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, 200, getResponse.StatusCode(), "getting should be successful")
+	})
+
+	t.Run("cant get comment - missing id", func(t *testing.T) {
+		client := resty.New()
+		getResponse, err := client.R().Get("http://localhost:8080/api/v1/comment/")
+		assert.NoError(t, err)
+		assert.Equal(t, 404, getResponse.StatusCode())
 	})
 }
